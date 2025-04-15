d46663ae62714fc17d47b434f012fd4105ff90b2
ZabolotnyiGit <zabolotnyi.pm@gmail.com>
2025-04-15 09:49:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-TKLMJSN7ZQ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-TKLMJSN7ZQ');
-</script>
-
-<script>
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -42,4 +32,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-</script>
